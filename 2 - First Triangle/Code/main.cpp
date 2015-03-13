385bf71a67db3349a8ff29029ee2fae1675084a2
Kim Restad <kim.restad@gmail.com>
2015-03-13 18:26:14 +0100

    Finished sample, drawing triangle. No comments!
    Need to look over sample to make sure it's minimal.

--- 2 - First Triangle/Code/main.cpp
+++ 2 - First Triangle/Code/main.cpp
@@ -1,5 +1,5 @@
 // ######################################################################################
-// ## A minimal working sample creating and showing a basic window in Windows.
+// ## A minimal working sample setting up DirectX 11 and drawing a simple triangle.
 // ##
 // ## Copyright (c) <2015> <Tim Henriksson and Kim Restad>
 // ## 
@@ -22,21 +22,47 @@
 // ######################################################################################
 
 #include <Windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
 
-// Forward declarations.
-void Initialise();
+// Link to needed lib files. Can also be done by adding these to Properties -> Linker -> Input -> Additional Dependencies
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
+
+// Window forward declarations.
+void InitialiseWindow();
 void Run();
 LRESULT CALLBACK WindowProcedure(HWND handle, UINT message, WPARAM wParam, LPARAM lParam);
 
+// DirectX forward declarations.
+void InitialiseDirectX();
+void CreateDeviceAndSwapChain();
+void CreateRenderTargetView();
+void CreateViewport();
+void CreateShaders();
+void Render();
+
+// Window global variables.
 HWND gWindowHandle = 0;
+int gWindowWidth = 800;
+int gWindowHeight = 600;
+ID3D11VertexShader* gVertexShader = nullptr;
+ID3D11PixelShader* gPixelShader = nullptr;
+
+// DirectX global variables.
+ID3D11Device* gDevice = nullptr;
+ID3D11DeviceContext* gContext = nullptr;
+IDXGISwapChain* gSwapChain = nullptr;
+ID3D11RenderTargetView* gRTV = nullptr;
 
 void main()
 {
-	Initialise();
+	InitialiseWindow();
+	InitialiseDirectX();
 	Run();
 }
 
-void Initialise()
+void InitialiseWindow()
 {
 	HINSTANCE applicationHandle = GetModuleHandle(NULL);
 	WNDCLASS windowClass;
@@ -45,10 +71,10 @@ void Initialise()
 	windowClass.cbClsExtra = 0;						// Extra memory slots. We don't need this.
 	windowClass.cbWndExtra = 0;						// Extra memory slots. We don't need this.
 	windowClass.hInstance = applicationHandle;		// Handle to the instance containing the class's window procedure.
-	windowClass.hIcon = LoadIcon(0, IDI_APPLICATION);		// Load the default window icon.
-	windowClass.hCursor = LoadCursor(0, IDC_ARROW);			// Load the default arrow curser.
-	windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	// Set the window background to white.
-	windowClass.lpszMenuName = 0;					// The window has no menu.
+	windowClass.hIcon = LoadIcon(0, IDI_APPLICATION);				// Load the default window icon.
+	windowClass.hCursor = LoadCursor(0, IDC_ARROW);					// Load the default arrow curser.
+	windowClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));	// Set the window background to white.
+	windowClass.lpszMenuName = NULL;				// The window has no menu.
 	windowClass.lpszClassName = L"WindowClass";		// Define a name for this window class.
 
 	RegisterClass(&windowClass);
@@ -59,12 +85,12 @@ void Initialise()
 		WS_OVERLAPPEDWINDOW,		// Window style: overlapped is a combination of several styles.
 		100,						// The window's x position in pixels from the main monitor's upper left corner.
 		100,						// The window's y position in pixels from the main monitor's upper left corner.
-		800,						// The window's width in pixels.
-		600,						// The window's height in pixels.
-		0,							// No parent - our window is the only one.
-		0,							// No menu so there is no handle to one.
+		gWindowWidth,				// The window's width in pixels.
+		gWindowHeight,				// The window's height in pixels.
+		NULL,						// No parent - our window is the only one.
+		NULL,						// No menu so there is no handle to one.
 		applicationHandle,			// Handle to the application owning the window.
-		0							// Info sent to the WM_CREATE message through its lParam member.
+		NULL						// Info sent to the WM_CREATE message through its lParam member.
 		);
 
 	// Window is not shown by default, so ShowWindow must be called for the window passing in how to show the window 
@@ -79,10 +105,14 @@ void Run()
 
 	while (windowMsg.message != WM_QUIT)
 	{
-		if (PeekMessage(&windowMsg, 0, 0, 0, PM_REMOVE))
+		if (PeekMessage(&windowMsg, NULL, NULL, NULL, PM_REMOVE))
 		{
 			TranslateMessage(&windowMsg);	// Prepare the message.
 			DispatchMessage(&windowMsg);	// Send the message to the window procedure.
+		}
+		else
+		{
+			Render();
 		}
 	}
 }
@@ -98,4 +128,147 @@ LRESULT CALLBACK WindowProcedure(HWND handle, UINT message, WPARAM wParam, LPARA
 
 	// If a message has not been handled, send it to the default window procedure for handling.
 	return DefWindowProc(handle, message, wParam, lParam);
+}
+
+void InitialiseDirectX()
+{
+	CreateDeviceAndSwapChain();
+	CreateRenderTargetView();
+	CreateViewport();
+	CreateShaders();
+}
+
+void CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC scDesc;
+	scDesc.BufferDesc.Width = gWindowWidth;
+	scDesc.BufferDesc.Height = gWindowHeight;
+	scDesc.BufferDesc.RefreshRate.Numerator = 60;
+	scDesc.BufferDesc.RefreshRate.Denominator = 1;
+	scDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scDesc.SampleDesc.Count = 1;
+	scDesc.SampleDesc.Quality = 0;
+	scDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scDesc.BufferCount = 1;
+	scDesc.OutputWindow = gWindowHandle;
+	scDesc.Windowed = true;
+	scDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scDesc.Flags = 0;
+
+	D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		nullptr,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scDesc,
+		&gSwapChain,
+		&gDevice,
+		nullptr,
+		&gContext
+		);
+}
+
+void CreateRenderTargetView()
+{
+	ID3D11Texture2D* backbuffer;
+	gSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&backbuffer));
+	gDevice->CreateRenderTargetView(backbuffer, nullptr, &gRTV);
+	backbuffer->Release();
+
+	gContext->OMSetRenderTargets(1, &gRTV, nullptr);
+}
+
+void CreateViewport()
+{
+	D3D11_VIEWPORT vp;
+	vp.TopLeftX = 0.0f;
+	vp.TopLeftY = 0.0f;
+	vp.Width = static_cast<float>(gWindowWidth);
+	vp.Height = static_cast<float>(gWindowHeight);
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+
+	gContext->RSSetViewports(1, &vp);
+}
+
+void CreateShaders()
+{
+	// Define shader contents
+	const char* vertexShader = R"(
+		float4 main(uint id : SV_VERTEXID) : SV_POSITION
+		{
+			return float4(-0.5f + (float)(id % 2), -0.5f + (id == 0), 0.0f, 1.0f);
+		}
+		)";
+
+	const char* pixelShader = R"(
+		float4 main(float4 position : SV_POSITION) : SV_TARGET
+		{
+			return float4(1.0f, 0.0f, 0.0f, 1.0f);
+		}
+		)";
+
+	// Compile and create vertex shader
+	ID3DBlob* compiledShader = nullptr;
+	D3DCompile(
+		reinterpret_cast<LPCVOID>(vertexShader),
+		strlen(vertexShader),
+		NULL,
+		nullptr,
+		nullptr,
+		"main",
+		"vs_5_0",
+		0,
+		0,
+		&compiledShader,
+		nullptr
+		);
+
+	gDevice->CreateVertexShader(
+		compiledShader->GetBufferPointer(),
+		compiledShader->GetBufferSize(),
+		NULL,
+		&gVertexShader
+		);
+
+	// Compile and create pixel shader
+	compiledShader = nullptr;
+	D3DCompile(
+		reinterpret_cast<LPCVOID>(pixelShader),
+		strlen(pixelShader),
+		NULL,
+		nullptr,
+		nullptr,
+		"main",
+		"ps_5_0",
+		0,
+		0,
+		&compiledShader,
+		nullptr
+		);
+
+	gDevice->CreatePixelShader(
+		compiledShader->GetBufferPointer(),
+		compiledShader->GetBufferSize(),
+		NULL,
+		&gPixelShader
+		);
+}
+
+void Render()
+{
+	FLOAT bgColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	gContext->ClearRenderTargetView(gRTV, bgColor);
+
+	gContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	gContext->VSSetShader(gVertexShader, NULL, NULL);
+	gContext->PSSetShader(gPixelShader, NULL, NULL);
+
+	gContext->Draw(3, 0);
+	gSwapChain->Present(0, 0);
 }
